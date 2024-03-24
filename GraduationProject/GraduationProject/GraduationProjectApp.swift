d04f01d876437a23367635268e5e14841b3d2339
ulasozturk <ulasozturkdev@gmail.com>
2024-03-24 15:05:06 +0300

    project set up

--- GraduationProject/GraduationProject/GraduationProjectApp.swift
+++ GraduationProject/GraduationProject/GraduationProjectApp.swift
@@ -1,20 +1,26 @@
-//
-//  GraduationProjectApp.swift
-//  GraduationProject
-//
-//  Created by macbook pro on 24.03.2024.
-//
 
+
+import FirebaseCore
 import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool
+  {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
 struct GraduationProjectApp: App {
-    let persistenceController = PersistenceController.shared
+  let persistenceController = PersistenceController.shared
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
 
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+        .environment(\.managedObjectContext, persistenceController.container.viewContext)
     }
+  }
 }
---
+++ GraduationProject/GraduationProject/Service/FirebaseService.swift
@@ -0,0 +1,8 @@
+//
+//  FirebaseService.swift
+//  GraduationProject
+//
+//  Created by macbook pro on 24.03.2024.
+//
+
+import Foundation
